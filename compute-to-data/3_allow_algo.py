1860c22c42592017f5de677a675d2aa1661eeecd
Akshay <akshay.ap95@gmail.com>
2022-08-07 23:39:27 +0200

    Update config

--- compute-to-data/3_allow_algo.py
+++ compute-to-data/3_allow_algo.py
@@ -1,12 +1,13 @@
-import imp
 from config import web3_wallet, ocean, config
 from ocean_lib.aquarius import Aquarius
-from ocean_lib.web3_internal.wallet import Wallet
-import os
 
-dataset_did = "did:op:33e68473055b7f84be2d4f6baea2bf58bb97e87ab683f67571e237c7baae3fe4"
-algo_did = "did:op:656b3ab786fc76255988d8c106c7df6db8af9fada6a5b5f9989377da05f8827a"
+# Local
+# dataset_did = "did:op:33e68473055b7f84be2d4f6baea2bf58bb97e87ab683f67571e237c7baae3fe4"
+# algo_did = "did:op:656b3ab786fc76255988d8c106c7df6db8af9fada6a5b5f9989377da05f8827a"
 
+# Mumbai
+dataset_did = "did:op:788b00d5e066255fa7aca69919a28727abe63e6a8d286e69a008fe3af7372413"
+algo_did = "did:op:21b72442f37169bb3f0446a86ff31693cb23294b546e4d4c43b2a908aa7f164d"
 
 aquarius = Aquarius.get_instance(config.metadata_cache_uri)
 
--- config.py
+++ config.py
@@ -1,28 +1,32 @@
 import os
 from dotenv import load_dotenv
 from ocean_lib.web3_internal.wallet import Wallet
-from ocean_lib.example_config import ExampleConfig, get_config_dict
+from ocean_lib.example_config import ExampleConfig
 from ocean_lib.ocean.ocean import Ocean
-from ocean_lib.ocean.util import get_web3
+from web3.middleware import geth_poa_middleware
 
 load_dotenv()
 
-import os
 from ocean_lib.ocean.ocean import Ocean
-d = {
-   'network' : os.getenv('OCEAN_NETWORK_URL'),
-   'metadataCacheUri' : os.getenv('METADATA_CACHE_URI'),
-   'providerUri' : os.getenv('PROVIDER_URL'),
-   'network_name': os.getenv('OCEAN_NETWORK')
-}
-# ocean = Ocean(d)
 
 config = ExampleConfig.get_config()
 ocean = Ocean(config)
 
-print("*" *100)
-print("ocean.config.address_file", ocean.config.address_file)
-print("ocean.config.network_name", ocean.config.network_name)
-print("*" *100)
-user_private_key = os.getenv('PRIVATE_KEY')
-web3_wallet = Wallet(ocean.web3, user_private_key, ocean.config.block_confirmations, ocean.config.transaction_timeout)
+user_private_key = os.getenv("PRIVATE_KEY")
+
+if config.network_name == "mumbai":
+    ocean.web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+web3_wallet = Wallet(
+    ocean.web3,
+    user_private_key,
+    ocean.config.block_confirmations,
+    ocean.config.transaction_timeout,
+)
+
+web3_wallet2 = Wallet(
+    ocean.web3,
+    os.getenv("PRIVATE_KEY2"),
+    config.block_confirmations,
+    config.transaction_timeout,
+)
